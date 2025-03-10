72e5bdd37ed98321a39b7737fb5f6e73eb383ce7
keronabil20 <keronabil55551@gmail.com>
2025-03-10 14:04:04 +0200

    add const and sve routes pages

---
+++ lib/constants/strings.dart
@@ -0,0 +1,6 @@
+import 'package:reck_morty/presentation/screens/detailed_characters.dart';
+
+const baseUrl = "https://rickandmortyapi.com/api";
+
+const characterScreen = '/';
+const detailedCharacters = '/detail_character';
